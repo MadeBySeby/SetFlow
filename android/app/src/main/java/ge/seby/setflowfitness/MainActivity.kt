33d05375bb3742e3618e7ebe420e7d97d46d37f1
dato sebiskveradze <david.katsitadze.1@btu.edu.ge>
2026-01-06 19:25:40 +0400

    Refactor code structure for improved readability and maintainability

--- android/app/src/main/java/ge/seby/setflowfitness/MainActivity.kt
+++ android/app/src/main/java/ge/seby/setflowfitness/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.SetFlow
+package ge.seby.setflowfitness
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/ge/seby/setflowfitness/MainApplication.kt
+++ android/app/src/main/java/ge/seby/setflowfitness/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.SetFlow
+package ge.seby.setflowfitness
 
 import android.app.Application
 import android.content.res.Configuration
